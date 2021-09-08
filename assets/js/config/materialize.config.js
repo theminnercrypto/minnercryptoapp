d43479245940bbc98dd8fa6fc4a12e6c2003f34c
theminnercrypto <87697616+theminnercrypto@users.noreply.github.com>
2021-09-08 15:37:51 -0400

    asdasdasdasd
    asdasdasdasdsd

--- assets/js/config/materialize.config.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const modals = document.querySelectorAll(".modal");
-  M.Modal.init(modals);
-
-  const items = document.querySelectorAll(".collapsible");
-  M.Collapsible.init(items);
-});
